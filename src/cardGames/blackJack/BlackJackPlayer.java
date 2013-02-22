287691aa63c3019c1ed2d792f29314420e2fdd5f
Chimney42 <draggili@gmail.com>
2013-02-22 22:58:52 +0100

    implements BlackJackPlayer with tests

--- src/cardGames/blackJack/BlackJackPlayer.java
+++ src/cardGames/blackJack/BlackJackPlayer.java
@@ -1,8 +1,31 @@
 package cardGames.blackJack;
 
+import cardGames.Player;
+
 /**
  * Author: lian
  * Date: 2/22/13
+ * This is a Player implementation specific for BlackJack, since each Player has a certain number of chips
  */
-public class BlackJackPlayer {
+
+public class BlackJackPlayer extends Player {
+
+    private int chipCount;
+
+    public BlackJackPlayer() {
+        super();
+        this.chipCount = 0;
+    }
+    public BlackJackPlayer(String name) {
+        super(name);
+        this.chipCount = 0;
+    }
+
+    public void addChips(int chips) {
+        this.chipCount += chips;
+    }
+
+    public int getChipCount() {
+        return this.chipCount;
+    }
 }
--- tests/cardGames/blackJack/BlackJackPlayerTest.java
+++ tests/cardGames/blackJack/BlackJackPlayerTest.java
@@ -1,8 +1,24 @@
 package cardGames.blackJack;
 
+import cardGames.Player;
+import junit.framework.*;
+import org.junit.*;
+import org.junit.Test;
+
 /**
  * Author: lian
  * Date: 2/22/13
  */
-public class BlackJackPlayerTest {
+public class BlackJackPlayerTest extends TestCase {
+
+    @Test
+    public void testChipcountShouldIncreaseByNumberOfGivenChips(){
+        BlackJackPlayer player = new BlackJackPlayer();
+        int givenChips = 5;
+
+        player.addChips(givenChips);
+
+        assertEquals(givenChips, player.getChipCount());
+    }
+
 }
