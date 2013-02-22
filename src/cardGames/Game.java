de43177ea5e11dcfee7fa774728361bcbc263d6c
Chimney42 <draggili@gmail.com>
2013-02-22 23:00:59 +0100

    adds interfaces

---
+++ src/cardGames/Game.java
@@ -0,0 +1,14 @@
+package cardGames;
+
+/**
+ * Author: lian
+ * Date: 2/22/13
+ * All Games have to implement this interface
+ */
+public interface Game {
+
+    //Initialize game
+    public void init(GameState gameState);
+
+    public GameState getInitialGameState();
+}
---
+++ src/cardGames/blackJack/BlackJack.java
@@ -0,0 +1,30 @@
+package cardGames.blackJack;
+
+import cardGames.Game;
+import cardGames.GameState;
+
+/**
+ * Author: lian
+ * Date: 2/22/13
+ * All varieties of Black Jack have to implement this Interface
+ * This interface only holds those methods, which are needed to play Black Jack
+ */
+public interface BlackJack extends Game {
+
+    //Initialize game
+    public void init(GameState gameState);
+
+    // On Startup, a full random deck is used and 2 cards are given to each player
+    public void giveCards();
+
+    // Each player has the possibility to add one additional card to his hand, when it's his turn
+    public void addCard();
+
+    // Each player can end his turn
+    public void endTurn();
+
+    //After all players have ended their turns, the winner will be determined
+    public void checkHands();
+
+    public BlackJackGameState getInitialGameState();
+}
